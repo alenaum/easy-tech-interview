7d113943ab22107f5efa1d3009665a04bd6d0e1c
alex.naumov <alexander.naumov.n@gmail.com>
2023-11-18 21:56:58 +0100

    Revert "reverted back to simple blog"
    This reverts commit 6652ff9e936402727ae74bd3106ab271ad95802c.

--- components/alert.tsx
+++ components/alert.tsx
@@ -9,10 +9,7 @@ type Props = {
 const Alert = ({ preview }: Props) => {
   return (
     <div
-      className={cn('border-b', {
-        'bg-neutral-800 border-neutral-800 text-white': preview,
-        'bg-neutral-50 border-neutral-200': !preview,
-      })}
+      className={cn('border-b')}
     >
       <Container>
         <div className="py-2 text-center text-sm">
@@ -29,14 +26,7 @@ const Alert = ({ preview }: Props) => {
             </>
           ) : (
             <>
-              The source code for this blog is{' '}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-blue-600 duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
+              🚧 The website is under active construction 🚧
             </>
           )}
         </div>
