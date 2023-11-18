33b49ce257e692902eb4f6307106a0f7905a179c
alex.naumov <alexander.naumov.n@gmail.com>
2023-11-18 10:47:52 +0100

    modified next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
+  themeConfig: './theme.config.tsx'
 })
 
-module.exports = withNextra()
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
+
+
+module.exports = withNextra(nextConfig);
